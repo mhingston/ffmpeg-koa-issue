9cd0f75c62c7420fd45fe3beddef6f9a13f05d04
Miles Hingston <miles@jumpkick.io>
2016-09-30 18:23:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const Koa = require('koa');
+const ffmpeg = require('fluent-ffmpeg');
+
+const app = new Koa();
+
+app.use((ctx, next) =>
+{
+    ctx.type = 'mp3';
+    ctx.response.attachment('audio.mp3');
+    const videoStream = fs.createReadStream('./video.m4a');
+    ffmpeg(videoStream)
+    .withAudioCodec('libmp3lame')
+    .toFormat('mp3')
+    .pipe(ctx.body, {end: true});
+});
+
+app.listen(3000);
