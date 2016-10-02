da615a6fee8a013eeda8936b2795428291f65eaa
Miles Hingston <mhingston@users.noreply.github.com>
2016-10-02 13:45:42 +0100

    Fixed issue using PassThrough stream

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const stream = require('stream');
 const fs = require('fs');
 const Koa = require('koa');
 const ffmpeg = require('fluent-ffmpeg');
@@ -8,6 +9,7 @@ app.use((ctx, next) =>
 {
     ctx.type = 'mp3';
     ctx.response.attachment('audio.mp3');
+    ctx.body = stream.PassThrough();
     const videoStream = fs.createReadStream('./video.m4a');
     ffmpeg(videoStream)
     .withAudioCodec('libmp3lame')
